e3e095475b04f1a071bb568a32b8fef0a6658c56
Arthur CARRIERE <a.carriere98@gmail.com>
2018-03-21 12:00:56 -0400

    Preparation to merge

--- PAD Engine/Sources/Main.cpp
+++ PAD Engine/Sources/Main.cpp
@@ -12,36 +12,12 @@
 
 using namespace pad;
 
-inline void ArthurThreadingTest()
-{
-	std::chrono::high_resolution_clock::time_point Start
-		= std::chrono::high_resolution_clock::now();
-	
-	trp::ThreadPool	Pool(4);
-	std::mutex m;
-	for (std::uint32_t i = 0u; i < 80u; ++i)
-	{
-		Pool.Push([&m, &Start]()
-		{
-			std::this_thread::sleep_for(std::chrono::seconds(1));
-			auto End = std::chrono::high_resolution_clock::now();
-			m.lock();
-			std::cout << std::chrono::duration<float>(End - Start).count() << std::endl;
-			m.unlock();
-		});
-	}
-	auto End = std::chrono::high_resolution_clock::now();
-	std::cout << std::chrono::duration<float>(End - Start).count() << std::endl;
-	while (true);
-}
-
 int main()
 {
-	ArthurThreadingTest();
-	/*core::Engine engine;
+	core::Engine engine;
 	
 	engine.InitSimulation();
-	engine.StartSimulation();*/
+	engine.StartSimulation();
 
 
 	return EXIT_SUCCESS;

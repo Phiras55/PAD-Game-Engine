11f4b767ab445b5fca662d01dd9dee8fa3032efc
Unknown <a.carriere98@gmail.com>
2018-04-27 03:06:13 -0400

    Added json

--- GameTest/Sources/main.cpp
+++ GameTest/Sources/main.cpp
@@ -2,6 +2,7 @@
 
 #include <Graphics/Model/Mesh.h>
 #include <tuple>
+#include <Utilities\Serialization\Serialization.h>
 
 
 class toto
@@ -18,6 +19,13 @@ int main()
 	t.a = 6.2;
 	t.b = 3.1f;
 	t.c = 1;
+	
+	json j;
+	AddToJson(j, "a", t.a);
+	AddToJson(j, "b", t.b);
+	AddToJson(j, "c", t.c);
+	auto f = CreateFile("toto.json", j);
+	f.close();
 
 	system("pause");
 
--- PAD Engine/Sources/Utilities/Serialization/Serialization.h
+++ PAD Engine/Sources/Utilities/Serialization/Serialization.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <json-develop\single_include\nlohmann\json.hpp>
+#include <fstream>
+
+using json = nlohmann::json;
 
 template<typename T>
 struct ISerializable
 {
-	virtual std::string Serialize()	= 0;
-	virtual T Deserialize()	= 0;
+	void Serialize(std::string path)
+	{
+		CreateFile(path, SerializeData());
+	}
+	virtual json SerializeData()	= 0;
+	virtual T Deserialize()			= 0;
 };
+
+void CreateFile(std::string path, json j)
+{
+	std::ofstream f(path);
+	f << j;
+	f.close();
+}
+
+template<typename T>
+void AddToJson(json& j, std::string name, T value)
+{
+	j[name] = value;
+}

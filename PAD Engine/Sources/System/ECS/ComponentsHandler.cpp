6388bc86826c55fe01538a91b9260c6eb7a82112
Unknown <philippe.campion.gingras@hotmail.com>
2018-04-29 14:33:26 -0400

    Stash for merge with develop and then to start a new feature

---
+++ PAD Engine/Sources/System/ECS/ComponentsHandler.cpp
@@ -0,0 +1,20 @@
+#include <EnginePCH.h>
+#include <System/ECS/ComponentsHandler.h>
+
+namespace pad	{
+namespace sys	{
+namespace ecs	{
+
+ComponentsHandler::ComponentsHandler()
+{
+
+}
+
+ComponentsHandler::~ComponentsHandler()
+{
+
+}
+
+} // namespace ecs
+} // namespace sys
+} // namespace pad
--- PAD Engine/Sources/System/ECS/RigidBody.cpp
+++ PAD Engine/Sources/System/ECS/RigidBody.cpp
@@ -6,65 +6,6 @@ namespace pad	{
 namespace sys	{
 namespace ecs	{
 
-RigidBody::RigidBody()
-{
-	m_type = COMPONENT_TYPE::RIGIDBODY;
-}
-
-RigidBody::~RigidBody()
-{
-
-}
-
-void RigidBody::Init(PADObject* const _owner)
-{
-	m_owner = _owner;
-}
-
-void RigidBody::Start()
-{
-	btCollisionShape* collider;
-
-	for (auto comp : m_owner->GetComponents())
-	{
-		if (comp->GetType() == COMPONENT_TYPE::COLLIDER)
-		{
-
-		}
-	}
-	//btTransform btTrans;
-	//btTrans.setOrigin(btVector3(m_owner->GetTransform().Position().x,
-	//							m_owner->GetTransform().Position().y,
-	//							m_owner->GetTransform().Position().z));
-	//
-	//m_btMotionState = new btDefaultMotionState(btTrans);
-}
-
-void RigidBody::Update()
-{
-	// rigidBody.transform = parent.transform
-}
-
-void RigidBody::FixedUpdate()
-{
-
-}
-
-void RigidBody::LateUpdate()
-{
-	// parent.transform = rigidBody.transform
-}
-
-void RigidBody::SetMass(const float _mass)
-{
-	// TO DO
-}
-
-} // namespace ecs
-} // namespace sys
-} // namespace pad
-namespace ecs	{
-
 RigidBody::RigidBody() :
 	m_collider(nullptr),
 	m_btMotionState(nullptr),
@@ -173,4 +114,8 @@ inline void RigidBody::SetCollider(ACollider* const _collider)
 {
 	m_collider = _collider;
 }
-
+
+} // namespace ecs
+} // namespace sys
+} // namespace pad
+
